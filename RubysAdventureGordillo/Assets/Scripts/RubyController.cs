a9e4be3dcbe505b9996328d96f9ea36952f738e1
Maximiliano Gordillo <724910@student.dusd.net>
2023-11-20 16:45:44 -0800

    finished ruby's health
    finished ruby's health script and added a strawberry.

--- RubysAdventureGordillo/Assets/Scripts/RubyController.cs
+++ RubysAdventureGordillo/Assets/Scripts/RubyController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 10.0f;
+
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth;
     
     Rigidbody2D rigidbody2d;
@@ -15,8 +18,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
+        currentHealth = 1;
     }
     
     // Update is called once per frame
@@ -29,13 +32,13 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 10.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 10.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
