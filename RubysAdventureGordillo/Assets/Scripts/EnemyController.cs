d2f20b93a8160b1bc9a5d79de529a40ce2cf51df
Maximiliano Gordillo <724910@student.dusd.net>
2023-11-21 23:52:24 -0800

    Animations
    finished animations at a time i should be asleep. oh my...

--- RubysAdventureGordillo/Assets/Scripts/EnemyController.cs
+++ RubysAdventureGordillo/Assets/Scripts/EnemyController.cs
@@ -12,11 +12,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -36,11 +39,15 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction; ;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed* direction; ;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y,", 0);
+            position.x = position.x + Time.deltaTime * speed* direction;
         }
         
         rigidbody2D.MovePosition(position);
