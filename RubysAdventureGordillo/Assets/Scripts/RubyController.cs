0e86dc1c60be9e17093ae8c6c7d73830530ffb08
Maximiliano Gordillo <724910@student.dusd.net>
2023-11-21 18:31:44 -0800

    added damaging
    added damaging scripts and a bot.

--- RubysAdventureGordillo/Assets/Scripts/RubyController.cs
+++ RubysAdventureGordillo/Assets/Scripts/RubyController.cs
@@ -7,8 +7,12 @@ public class RubyController : MonoBehaviour
     public float speed = 10.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
     
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -19,7 +23,6 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
     }
     
     // Update is called once per frame
@@ -27,6 +30,15 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -40,6 +52,17 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+
+        isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
